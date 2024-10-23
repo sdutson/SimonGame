d30e1fe48b5db6ccfec9cf86b132c35b9c9b4917
sdutson <sdutson222@gmail.com>
2024-10-23 09:40:26 -0600

    Removed excess comments

--- controller.h
+++ controller.h
@@ -6,9 +6,6 @@
 
 class Controller : public QObject
 {
-    // TODO: Example code has Q_OBJECT here. Do we need it?
-    Q_OBJECT
-
     GameLogic model;
 
     std::vector<int>::iterator moveIterator;
@@ -24,7 +21,6 @@ class Controller : public QObject
     void playerClickedButton(int buttonValue);
 
     const int COMPUTER_TURN_LENGTH = 2000; // TODO: adjust this as needed.
-
 
 public:
     explicit Controller(QObject *parent = nullptr);

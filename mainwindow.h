d0c2e2a8113541bec5d937388eeb94368aa6157b
sdutson <sdutson222@gmail.com>
2024-10-23 07:37:20 -0600

    Added comment layout for potential slots.

--- mainwindow.h
+++ mainwindow.h
@@ -21,3 +21,10 @@ private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
+
+// Slots needed
+//  - For when player presses start button.
+//  - For when player presses redButton.(Light it up)
+//  - For when player presses blueButton (Light it up).
+//  - For when controller tells up to light up a button. (This can also work in tandem with the previous two slots.)
+//  - For when controller tells us to end the game.

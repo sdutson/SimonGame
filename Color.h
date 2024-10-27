00e98d784e84c5b5efd430069c185f2dfead5e4c
sdutson <sdutson222@gmail.com>
2024-10-27 15:28:53 -0600

    Added header comment

--- Color.h
+++ Color.h
@@ -1,10 +1,12 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+/// @brief - This class represents the IDs for the color buttons.
+/// @author - Samuel Dutson & Alexander Kuettel
+/// Assign 06
 enum class Color {
     RED = 0,
     BLUE = 1
 };
-// TODO: Move this into the controller? 
 
 #endif // COLOR_H

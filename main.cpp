6621f1e8073c5f9beda1d4c8a09d5b26fe2455f2
sdutson <sdutson222@gmail.com>
2024-10-28 12:11:14 -0600

    Cleaned up comments

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 /// @brief - Sets up and starts the GUI application. 
 /// @author - Samuel Dutson & Alexander Kuettel 
 /// Assign 06
+/// Github authors: sdutson and alexanderkuettel
+/// Github link: https://github.com/University-of-Utah-CS3505/a6-simon-f24-alexanderkuettel
+/// Creative Element: For our creative element we added a button that allowed the player to reset the round(Once per game).
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
